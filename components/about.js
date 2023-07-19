ad042c4d01a8773b42edc844e03f9b8090919e3a
unknown <jonikarell1@gmail.com>
2023-07-19 15:01:21 +0700

    modified layout

--- components/about.js
+++ components/about.js
@@ -36,12 +36,12 @@ const About = () => {
             <div className='bg-white dark:bg-stone-950  px-20 md:px-52 pt-36 pb-10'>
                 <h1 data-aos='fade-up' data-aos-delay='150' className='text-center font-bold text-2xl md:text-5xl mb-20 dark:text-neutral-100'>About</h1>
                 <div className='grid grid-cols-1 lg:grid-cols-4 lg:gap-10 shadow-lg p-5 border rounded-xl'>
-                    <div className=' w-32 h-32 justify-self-center self-center col-span-1 rounded-xl overflow-hidden shadow-2xl' >
+                    <div className=' w-32 h-32 justify-self-center self-center col-span-1 rounded-xl overflow-hidden' >
                         <Image data-aos='fade-right' data-aos-delay='200' src={'/static/images/YGBM8705.JPG'} width={100} height={100} alt='profile' className='w-full h-full object-cover  object-center aos-init' />
 
                     </div>
                     <div className='col-span-2 ' >
-                        {/* <h1 data-aos='fade-right' data-aos-delay='150' className='text-center font-bold text-2xl md:text-5xl mb-20 dark:text-neutral-100'>About</h1> */}
+
                         <h1 data-aos='fade-in' data-aos-delay='200' className='font-bold text-xl text-center lg:text-left md:text-3xl mb-5 text-slate-900 dark:text-neutral-100 mt-5 lg:mt-0'>My Name is Ripa Miptah Salam</h1>
                         <p data-aos='fade-out' data-aos-delay='250' className=' text-sm lg:text-lg text-center lg:text-left text-slate-900 dark:text-neutral-100'  >I am an Associate Degree majoring in Electronics Engineering at Politeknik Negeri Bandung. Interested in web Developer and Internet of Things.</p>
                     </div>
